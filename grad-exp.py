102d49ccc968bafaeb984532c6765b4e46010221
Yuval.Solaz <yuval.solaz@gmail.com>
2019-03-17 09:10:27 +0200

    grad

---
+++ grad-exp.py
@@ -0,0 +1,46 @@
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input, decode_predictions
+import numpy as np
+import shap
+import keras.backend as K
+import json
+
+# load pre-trained model and choose two images to explain
+model = VGG16(weights='imagenet', include_top=True)
+X,y = shap.datasets.imagenet50()
+to_explain = X[[39,41]]
+
+# load the ImageNet class names
+url = "https://s3.amazonaws.com/deep-learning-models/image-models/imagenet_class_index.json"
+fname = shap.datasets.cache(url)
+with open(fname) as f:
+    class_names = json.load(f)
+
+# explain how the input to the 7th layer of the model explains the top two classes
+def map2layer(x, layer):
+    feed_dict = dict(zip([model.layers[0].input], [preprocess_input(x.copy())]))
+    return K.get_session().run(model.layers[layer].input, feed_dict)
+e = shap.GradientExplainer((model.layers[7].input, model.layers[-1].output), map2layer(preprocess_input(X.copy()), 7))
+shap_values,indexes = e.shap_values(map2layer(to_explain, 7), ranked_outputs=2)
+
+# get the names for the classes
+index_names = np.vectorize(lambda x: class_names[str(x)][1])(indexes)
+
+# plot the explanations
+shap.image_plot(shap_values, to_explain, index_names)
+
+# Explain with local smoothing
+
+# explain how the input to the 7th layer of the model explains the top two classes
+explainer = shap.GradientExplainer(
+    (model.layers[7].input, model.layers[-1].output),
+    map2layer(preprocess_input(X.copy()), 7),
+    local_smoothing=100
+)
+shap_values,indexes = explainer.shap_values(map2layer(to_explain, 7), ranked_outputs=2)
+
+# get the names for the classes
+index_names = np.vectorize(lambda x: class_names[str(x)][1])(indexes)
+
+# plot the explanations
+shap.image_plot(shap_values, to_explain, index_names)
